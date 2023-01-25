285b694171b08e570fdfd9cb0789bab394560adb
Diego Luna <slowkingvii@gmail.com>
2023-01-24 23:59:44 -0600

    Add base styles for Home page

--- src/Home.js
+++ src/Home.js
@@ -1,3 +1,10 @@
-const Home = () => <div>This is Home</div>;
+import LoremIpsum from 'react-lorem-ipsum';
+
+const Home = () => (
+  <div>
+    <h2>Welcome to our page!</h2>
+    <LoremIpsum p={2} />
+  </div>
+);
 
 export default Home;
