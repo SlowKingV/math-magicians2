38563b0f079cbf965282c3a06b1c4911db91a0ba
Diego Luna <slowkingvii@gmail.com>
2023-01-24 23:05:48 -0600

    Create Navigation component

---
+++ src/components/Navigation.js
@@ -0,0 +1,15 @@
+import { Link } from 'react-router-dom';
+
+const Navigation = () => (
+  <>
+    <nav>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/calculator">Calculator</Link></li>
+        <li><Link to="/quote">Quote</Link></li>
+      </ul>
+    </nav>
+  </>
+);
+
+export default Navigation;
