c4cdd41ab3468057a69e3a981067bbbf678f83a5
Diego Luna <slowkingvii@gmail.com>
2023-01-25 00:07:16 -0600

    Add base styles and content for Calculator page

--- src/CalcPage.js
+++ src/CalcPage.js
@@ -1,7 +1,10 @@
 import Calculator from './components/Calculator';
 
+import './css/CalcPage.css';
+
 const CalcPage = () => (
   <div className="content">
+    <h2>Let&apos;s do some math!</h2>
     <Calculator />
   </div>
 );
